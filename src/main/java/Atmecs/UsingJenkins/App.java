3b6c02d4997cfa4d24694472b9b5e0aa910c65c5
Chetan <chetan@ATMECSINDT-049.(none)>
2014-08-05 20:15:40 +0530

    initial commit for Jenkinssss

---
+++ src/main/java/Atmecs/UsingJenkins/App.java
@@ -0,0 +1,13 @@
+package Atmecs.UsingJenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Atmecs/UsingJenkins/TestClass.java
@@ -0,0 +1,15 @@
+/**
+ * @author chetan
+ *
+ */
+package Atmecs.UsingJenkins;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+	@Test
+public void draw() {
+System.out.println("drawing");
+
+}
+}
